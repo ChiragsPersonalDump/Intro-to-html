9253f7db87158fed253d34aaf4cfc19c76b8ac4c
Chirag Sharma <itschirag21@gmail.com>
2022-12-29 21:02:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+let thumb1 = document.getElementById("MB");
+thumb1.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=n2RNcPRtAiY&t=1s','_self')
+})
+let thumb2 = document.getElementById("MP");
+thumb2.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=mP0RAo9SKZk','_self')
+})
+let thumb3 = document.getElementById("SW");
+thumb3.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=FgjPQQeTh1w','_self')
+})
+let thumb4 = document.getElementById("VT");
+thumb4.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=094y1Z2wpJg&t=325s','_self')
+})
+let thumb5 = document.getElementById("CD");
+thumb5.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=86CQq3pKSUw','_self')
+})
+let thumb6 = document.getElementById("MBT");
+thumb6.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=yXWw0_UfSFg&t=1s','_self')
+})
+let thumb7 = document.getElementById("RLL");
+thumb7.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=fNVa1qMbF9Y&t=7s','_self')
+})
+let thumb8= document.getElementById("TV");
+thumb8.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=lFm4EM1juls','_self')
+})
+let thumb9 = document.getElementById("TX");
+thumb9.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=ixmxOlcrlUc&t=69s','_self')
+})
+let thumb10 = document.getElementById("BI");
+thumb10.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=R2vXbFp5C9o','_self')
+})
+let thumb11 = document.getElementById("DT");
+thumb11.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=0nZuYyXET3s','_self')
+})
+let thumb12 = document.getElementById("TE");
+thumb12.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=9iMGFqMmUFs&t=1s','_self')
+})
+let sidebarbutton0 = document.getElementById("explore");
+sidebarbutton0.addEventListener('click', () => {
+  window.open('http://127.0.0.1:5500/Youtube.html','_self')
+})
+let sidebarbutton1 = document.getElementById("explore");
+sidebarbutton1.addEventListener('click', () => {
+  window.open('https://www.youtube.com/feed/explore','_self')
+})
+
+let sidebarbutton2 = document.getElementById("subs");
+sidebarbutton2.addEventListener('click', () => {
+  window.open('https://www.youtube.com/feed/subscriptions','_self')
+})
+
+let tsidebarbutton3 = document.getElementById("orignals");
+tsidebarbutton3.addEventListener('click', () => {
+  window.open('https://www.youtube.com/channel/UCqVDpXKLmKeBU_yyt_QkItQ','_self')
+})
+
+let sidebarbutton4 = document.getElementById("ytmusic");
+sidebarbutton4.addEventListener('click', () => {
+  window.open('https://music.youtube.com/browse/FEmusic_language_selection','_self')
+})
+let sidebarbutton5 = document.getElementById("library");
+sidebarbutton5.addEventListener('click', () => {
+  window.open('https://www.youtube.com/feed/library','_self')
+})
+let searchicon = document.getElementById("searchicon");
+let searchbox = document.getElementById("searchbox");
+searchicon.addEventListener('click', () => {
+  window.open('https://www.youtube.com/results?search_query='+ searchbox.value,'_self')
+})
+
+
+
